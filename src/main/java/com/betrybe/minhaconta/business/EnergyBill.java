293e0a85cae65b27c6fd68b56d104f08799ebec0
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-04-04 16:24:49 -0300

    req 1

--- src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
@@ -1,5 +1,6 @@
 package com.betrybe.minhaconta.business;
 
+import com.ions.lightdealer.sdk.model.Address;
 import com.ions.lightdealer.sdk.model.ElectronicDevice;
 
 /**
@@ -7,10 +8,18 @@ import com.ions.lightdealer.sdk.model.ElectronicDevice;
  */
 public class EnergyBill {
   // Req. 1 – Create class constructor and attributes.
+  Address address;
+  boolean residentialPlan;
+  double rate = 0.15d;
 
+  public EnergyBill(Address address, boolean residentialPlan) {
+    this.address = address;
+    this.residentialPlan = residentialPlan;
+  }
   /**
    * Req. 2 – Calculates an adjusted tariff for non-residential plans.
    */
+
   public double adjustedTariff(double value) {
     return 0d;
   }
